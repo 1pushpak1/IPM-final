b505f4048183c6404ce05018258aedd8acff52fb
Pushpak <pushpakkumawat20028@gmail.com>
2023-08-24 19:49:09 +0530

    update

--- src/componets/pages/exams/Exams2.jsx
+++ src/componets/pages/exams/Exams2.jsx
@@ -35,15 +35,15 @@ function Exams2() {
                         <div className="tab-content" id="productTabContent">
                             <div className="course-description">
                                 <h5 className="h5">ELIGIBILITY CRITERIA</h5>
-                                <p>Candidates should have passed standard 10+2/ SSC. Candidates should have passed Standard XII/HSC or equivalent examination in 2021, and 2022 or appearing in 2023.</p>
+                                <p>• Candidates should have passed 10+2 examination in arts/ commerce/ science stream or equivalent with 60% (55% for candidates from SC/ ST/ PwD/ Transgender).</p>
+                                <p>• Candidates should have passed Standard XII/HSC or equivalent examination in 2021, and 2022 or appearing in 2023. The candidate must have passed the class 10th examination in the year not before 2019.</p>
                                 <h5 className="h5 mt-40">SELECTION & ADMISSION PROCESS</h5>
-                                <p>The candidates will be selected based on overall performance in the Aptitude Test (AT) and Personal Interview (PI).</p>
-                                <h5>Adminssion Weightage</h5>
-                                <p>Aptitude Test Score (ATS) – 65% AND Personal Interview (PI) – 35%</p>
+                                <p>The candidates, who scored more than or equal to category-wise sectional percentile 30, will be eligible for the Merit List. Weightage: JIPMAT Score – 95% & Gender Diversity – 5%</p>
+                                
                                 <h5>IMPORTANT DATES</h5>
-                                <p>Online Application starts from :09.03.2023</p>
-                                <p>Last date for submitting online application: 14.04.2023</p>
-                                <p>Exam Date : 16.06.2023</p>
+                                <p>Online Application starts from :06.04.2023</p>
+                                <p>Last date for submitting online application: 30.04.2023</p>
+                                <p>Exam Date : 28.05.2023</p>
                                 <h5>EXAM PATTERN</h5>
                                 <table className="table table-striped table-hover">
                                     <thead className='thead-dark'>
@@ -58,26 +58,26 @@ function Exams2() {
                                         <tr>
                                             <th scope="row">1</th>
                                             <td>Quantitative Ability- MCQ</td>
-                                            <td>40</td>
-                                            <td>160</td>
+                                            <td>33</td>
+                                            <td>132</td>
                                         </tr>
                                         <tr>
                                             <th scope="row">2</th>
-                                            <td> Quantitative Ability- SA (No Negative Marking)</td>
-                                            <td>40</td>
-                                            <td>160</td>
+                                            <td> Verbal Ability & Reading Comprehension</td>
+                                            <td>34</td>
+                                            <td>136</td>
                                         </tr>
                                         <tr>
                                             <th scope="row">3</th>
-                                            <td>Larry the Bird</td>
-                                            <td>40</td>
-                                            <td>160</td>
+                                            <td>Data Interpretation & Logical Reasoning</td>
+                                            <td>33</td>
+                                            <td>132</td>
                                         </tr>
                                         <tr>
                                             <th scope="row">4</th>
                                             <td>Total</td>
-                                            <td>120</td>
-                                            <td>180</td>
+                                            <td>100</td>
+                                            <td>400</td>
                                         </tr>
                                     </tbody>
                                 </table>
@@ -87,33 +87,32 @@ function Exams2() {
                                 </div>
                                 <div className="accordion-area accordion" id="faqAccordion">
                                     <div className="accordion-card">
-                                        <div className="accordion-header" id="collapse-item-1"><button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapse-1" aria-expanded="false" aria-controls="collapse-1">What Does It Take Excellent Author?</button></div>
+                                        <div className="accordion-header" id="collapse-item-1"><button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapse-1" aria-expanded="false" aria-controls="collapse-1">What Is The Duration Of The IPM Course At IIM Bodh Gaya?</button></div>
                                         <div id="collapse-1" className="accordion-collapse collapse" aria-labelledby="collapse-item-1" data-bs-parent="#faqAccordion">
                                             <div className="accordion-body">
-                                                <p className="faq-text">The time it takes to repair a roof depends on the extent of the damage. For minor repairs, it might take an hour or two. For significant repairs, a Edura or team might be at your home for half a day.</p>
+                                                <p className="faq-text">The IPM course at IIM Bodh Gaya is a five-year integrated program, which means it combines undergraduate and postgraduate studies.</p>
                                             </div>
                                         </div>
                                     </div>
                                     <div className="accordion-card active">
-                                    <div className="accordion-header" id="collapse-item-2"><button className="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapse-2" aria-expanded="true" aria-controls="collapse-2">Purpose of education is the integral development?</button></div>
+                                    <div className="accordion-header" id="collapse-item-2"><button className="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapse-2" aria-expanded="true" aria-controls="collapse-2">What Are The Specializations Offered In The IPM Course?</button></div>
                                     <div id="collapse-2" className="accordion-collapse collapse show" aria-labelledby="collapse-item-2" data-bs-parent="#faqAccordion">
                                         <div className="accordion-body">
-                                            <p className="faq-text">The time it takes to repair a roof depends on the extent of the damage. For minor repairs, it might take an hour or two. For significant repairs, a Edura or team might be at your home for half a day.</p>
+                                            <p className="faq-text">During the first three years of the IPM course, students will undergo a foundation program in management studies. In the last two years, students can choose from various specializations such as finance, marketing, operations, human resources, etc.</p>
                                         </div>
                                     </div>
                                     </div>
                                     <div className="accordion-card">
-                                    <div className="accordion-header" id="collapse-item-3"><button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapse-3" aria-expanded="false" aria-controls="collapse-3">Education can contribute to the betterment?</button></div>
+                                    <div className="accordion-header" id="collapse-item-3"><button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapse-3" aria-expanded="false" aria-controls="collapse-3">Is There Any Reservation Policy For Admissions To The IPM Course?</button></div>
                                     <div id="collapse-3" className="accordion-collapse collapse" aria-labelledby="collapse-item-3" data-bs-parent="#faqAccordion">
                                         <div className="accordion-body">
-                                            <p className="faq-text">The time it takes to repair a roof depends on the extent of the damage. For minor repairs, it might take an hour or two. For significant repairs, a Edura or team might be at your home for half a day.</p>
+                                            <p className="faq-text">Yes, IIM Bodh Gaya follows the reservation policy as per the norms of the Government of India. Reservation benefits are provided to candidates belonging to Scheduled Castes (SC), Scheduled Tribes (ST), Other Backward Classes (OBC), Economically Weaker Sections (EWS), and Persons with Disability (PwD).</p>
                                         </div>
                                     </div>
                                     </div>
                                 </div>
 
-                                <h5 className="h5 mt-40">Certification</h5>
-                                <p>It is designed to validate the knowledge, skills, and competencies of individuals in a specific area of study or professional field. The certification program is meticulously designed to ensure that candidates have acquired a comprehensive understanding of the subject matter. It encompasses both theoretical knowledge and practical application, allowing candidates to demonstrate their expertise in real-world scenarios.</p>
+                                
                             </div>
                         </div>
                      </div>
