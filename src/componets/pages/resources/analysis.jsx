b3300e1329693728f04c728fae0cea668998ac11
Pushpak <pushpakkumawat20028@gmail.com>
2023-09-03 10:32:25 +0530

    Update analysis.jsx

--- src/componets/pages/resources/analysis.jsx
+++ src/componets/pages/resources/analysis.jsx
@@ -9,10 +9,10 @@ function Analysis() {
          <div className="shape-mockup breadcumb-shape3 jump-reverse d-lg-block d-none" data-left="50px" data-bottom="80px"><img src="assets/img/bg/breadcumb_shape_1_3.png" alt="shape" /></div>
          <div className="container">
             <div className="breadcumb-content text-center">
-               <h1 className="breadcumb-title">Exam Calander</h1>
+               <h1 className="breadcumb-title">Exam Anlaysis</h1>
                <ul className="breadcumb-menu">
                   <li><a href="#">Resources</a></li>
-                  <li>Exam Calander</li>
+                  <li>Exam Analysis</li>
                </ul>
             </div>
          </div>
@@ -25,7 +25,7 @@ function Analysis() {
                      <div className="course-single-top">
                         {/* <div className="course-img"><img src="assets/img/course/course-details.png" alt="Course Image" /> </div> */}
                         {/* <div className="course-meta style2"><span><i className="fal fa-file"></i>Lesson 8</span> <span><i className="fal fa-chart-simple"></i>Beginner</span></div> */}
-                        <h2 className="course-title">Management Exam Calander</h2>
+                        <h2 className="course-title">Management Exam Analysis</h2>
                         <ul className="course-single-meta">
                            {/* <li className="course-single-meta-author"><img src="assets/img/course/author2.png" alt="author" /> <span><span className="meta-title">Instructor: </span><a href="#">Max Alexix</a></span></li>
                            <li><span className="meta-title">Last Update: </span><a href="#">20 Jun, 2023</a></li> */}
@@ -34,142 +34,64 @@ function Analysis() {
                      <div className="course-single-bottom">
                         <div className="tab-content" id="productTabContent">
                             <div className="course-description">
-                               <h5>IPMAT 2023 (Indore) Calendar</h5>
+                               <h5>SET 2023 Paper Structure</h5>
                                 <table className="table table-striped table-hover">
                                     <thead className='thead-dark'>
                                         <tr>
-                                            <th scope="col">S.No.</th>
-                                            <th scope="col">Milestone</th>
-                                            <th scope="col">Date/Deadline</th>
+                                            <th scope="col">Section</th>
+                                            <th scope="col">No. of Questions</th>
+                                            <th scope="col">Difficulty level</th>
                                             
                                         </tr>
                                     </thead>
                                     <tbody>
                                         <tr>
-                                            <th scope="row">1</th>
-                                            <td>IPMAT Application start date</td>
-                                            <td>6 March 2023</td>
+                                            <th scope="row">General English</th>
+                                            <td>16</td>
+                                            <td>Easy-Moderate</td>
+                                            
+                                        </tr>   
+                                        <tr>
+                                            <th scope="row">Quantitative Aptitude</th>
+                                            <td> 16</td>
+                                            <td>Moderate</td>
                                             
                                         </tr>
                                         <tr>
-                                            <th scope="row">2</th>
-                                            <td> IPMAT Application last date</td>
-                                            <td>14 April 2023</td>
+                                            <th scope="row">General Awareness</th>
+                                            <td>16</td>
+                                            <td>Easy-Moderate</td>
                                             
-                                        </tr>
+                                        </tr> 
                                         <tr>
-                                            <th scope="row">3</th>
-                                            <td>Exam Date</td>
-                                            <td>16 June 2023 (2 to 4 PM)</td>
-                                         
-                                        </tr>
-                                        <tr>
-                                            <th scope="row">4</th>
-                                            <td>Announcement of Interviews</td>
-                                            <td>End of June 2023</td>
-                                       
-                                        </tr>
-                                        <tr>
-                                            <th scope="row">4</th>
-                                            <td>Final Announcement of the result</td>
-                                            <td>Second week of July 2023</td>
-                                       
-                                        </tr>
+                                            <th scope="row">Analytical and Logical Reasoning</th>
+                                            <td>12</td>
+                                            <td>Easy-Moderate</td>
+                                            
+                                        </tr> 
+                                      
                                     </tbody>
                                 </table>
                           
                             </div>
-
-                            <div className="course-description">
-                                 <h5>IIM Ranchi Calendar</h5>
-                                <table className="table table-striped table-hover">
-                                    <thead className='thead-dark'>
-                                        <tr>
-                                            <th scope="col">S.No.</th>
-                                            <th scope="col">Milestone</th>
-                                            <th scope="col">Date/Deadline</th>
-                                            </tr>
-                                    </thead>
-                                    <tbody>
-                                        <tr>
-                                            <th scope="row">1</th>
-                                            <td>IPMAT Application start date</td>
-                                            <td>6 March 2023</td>
-                                            
-                                        </tr>
-                                        <tr>
-                                            <th scope="row">2</th>
-                                            <td> IPMAT Application last date</td>
-                                            <td>14 April 2023</td>
-                                            
-                                        </tr>
-                                        <tr>
-                                            <th scope="row">3</th>
-                                            <td>Exam Date</td>
-                                            <td>16 June 2023 (2 to 4 PM)</td>
-                                         
-                                        </tr>
-                                        <tr>
-                                            <th scope="row">4</th>
-                                            <td>Announcement of Interviews</td>
-                                            <td>End of June 2023</td>
-                                       
-                                        </tr>
-                                        <tr>
-                                            <th scope="row">4</th>
-                                            <td>Final Announcement of the result</td>
-                                            <td>Second week of July 2023</td>
-                                       
-                                        </tr>
-                                    </tbody>
-                                </table>
-                            </div>                
-                            <div className="course-description">
-                                 <h5>JIPMAT Exam Calendar</h5>
-                                <table className="table table-striped table-hover">
-                                    <thead className='thead-dark'>
-                                        <tr>
-                                            <th scope="col">S.No.</th>
-                                            <th scope="col">Milestone</th>
-                                            <th scope="col">Date/Deadline</th>
-                                            </tr>
-                                    </thead>
-                                    <tbody>
-                                        <tr>
-                                            <th scope="row">1</th>
-                                            <td>JIPMAT Application start date</td>
-                                            <td>6 April 2023</td>
-                                            
-                                        </tr>
-                                        <tr>
-                                            <th scope="row">2</th>
-                                            <td> JIPMAT Application last date</td>
-                                            <td>30 April 2023</td>
-                                            
-                                        </tr>
-                                        <tr>
-                                            <th scope="row">3</th>
-                                            <td>Correction Window</td>
-                                            <td>2-4 May 2023</td>
-                                         
-                                        </tr>
-                                        <tr>
-                                            <th scope="row">4</th>
-                                            <td>Exam Date</td>
-                                            <td>28 May 2023</td>
-                                       
-                                        </tr>
-                                        <tr>
-                                            <th scope="row">4</th>
-                                            <td>Final Announcement of the result</td>
-                                            <td>Second week of June 2023</td>
-                                       
-                                        </tr>
-                                    </tbody>
-                                </table>
-                            </div>   
-
-
+                            <h5 className="h5 mt-40">SET 2023 Detailed Analysis</h5>
+                                <p>The overall difficulty level of the paper was easy-moderate. Time constraint was not so much of a factor. There was ample time especially because of the difficulty level and the nature of the General Reasoning and English section. Find below the section-wise analysis of the paper.</p>
+                            <h3>General English</h3>
+                            <div className="col-md-auto">
+                        <div className="checklist">
+                           <ul>
+                                <li>Fill in the Blanks</li>
+                                <li>Synonyms/Antonyms</li>
+                                <li>Sentence Improvement</li>
+                                <li>Idioms and Phrases</li>
+                                <li>Error Spotting</li>
+                                <li>Spelling Errors</li>
+                                <li>Sentence Completion</li>
+                                <li>Sentence Re-arrangement</li>
+                                <li>Analogy</li>                      
+                           </ul>
+                        </div>
+                     </div>
 
                      </div>
                   </div>
