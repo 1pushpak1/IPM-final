3284597f2023ccca2c97c03fa4ffa03175b18811
Pushpak <pushpakkumawat20028@gmail.com>
2023-09-18 22:09:49 +0530

    Update Home.jsx

--- src/componets/pages/home/Home.jsx
+++ src/componets/pages/home/Home.jsx
@@ -180,80 +180,132 @@ function Home() {
 
                            </div>
                            <div className="tab-pane fade" id="instructor" role="tabpanel" aria-labelledby="instructor-tab">
-                              <div className="course-instructor">
-                                 <div className="course-author-box">
-                                    <div className="auhtor-img"><img src="assets/img/team/team_2_1.jpg" alt="Author Image" /></div>
-                                    <div className="media-body">
-                                       <h3 className="author-name"><a className="text-inherit" href="#">Kevin Perry</a></h3>
-                                       <p className="author-text">Optimize resource eveling innoation whereas visionary value. Compellingly engage extensible process with business process improvements.</p>
-                                       <div className="author-meta"><a href="#"><i className="fal fa-circle"></i>Master IIM Ahmedabad</a> <span><i className="fal fa-circle"></i>Bachlor Thapa University</span></div>
-                                       <div className="th-social"><a href="https://facebook.com/" target="_blank"><i className="fab fa-facebook-f"></i></a> <a href="https://twitter.com/" target="_blank"><i className="fab fa-twitter"></i></a> <a href="https://linkedin.com/" target="_blank"><i className="fab fa-linkedin-in"></i></a> <a href="https://instagram.com/" target="_blank"><i className="fab fa-instagram"></i></a></div>
-                                    </div>
-                                 </div>
-                              </div>
-                           </div>
-                           <div className="tab-pane fade" id="reviews" role="tabpanel" aria-labelledby="reviews-tab">
-                              <div className="course-Reviews">
-                                 <div className="th-comments-wrap">
-                                    <ul className="comment-list">
-                                       <li className="review th-comment-item">
-                                          <div className="th-post-comment">
-                                             <div className="comment-avater"><img src="assets/img/blog/comment-author-3.jpg" alt="Comment Author" /></div>
-                                             <div className="comment-content">
-                                                <h4 className="name">Mark Jack</h4>
-                                                <span className="commented-on"><i className="fal fa-calendar-alt"></i>22 April, 2022</span>
-                                                <div className="star-rating" role="img" aria-label="Rated 5.00 out of 5"><span style={{width:'100%'}}>Rated <strong className="rating">5.00</strong> out of 5 based on <span className="rating">1</span> customer rating</span></div>
-                                                <p className="text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco</p>
-                                             </div>
-                                          </div>
-                                       </li>
-                                       <li className="review th-comment-item">
-                                          <div className="th-post-comment">
-                                             <div className="comment-avater"><img src="assets/img/blog/comment-author-2.jpg" alt="Comment Author" /></div>
-                                             <div className="comment-content">
-                                                <h4 className="name">Alexa Deo</h4>
-                                                <span className="commented-on"><i className="fal fa-calendar-alt"></i>26 April, 2022</span>
-                                                <div className="star-rating" role="img" aria-label="Rated 5.00 out of 5"><span style={{width:'100%'}}>Rated <strong className="rating">5.00</strong> out of 5 based on <span className="rating">1</span> customer rating</span></div>
-                                                <p className="text">The purpose of lorem ipsum is to create a natural looking block of text (sentence, paragraph, page, etc.) that doesn't distract from the layout. A practice not without controversy, laying out pages.</p>
-                                             </div>
-                                          </div>
-                                       </li>
-                                       <li className="review th-comment-item">
-                                          <div className="th-post-comment">
-                                             <div className="comment-avater"><img src="assets/img/blog/comment-author-1.jpg" alt="Comment Author" /></div>
-                                             <div className="comment-content">
-                                                <h4 className="name">Tara sing</h4>
-                                                <span className="commented-on"><i className="fal fa-calendar-alt"></i>26 April, 2022</span>
-                                                <div className="star-rating" role="img" aria-label="Rated 5.00 out of 5"><span style={{width:'100%'}}>Rated <strong className="rating">5.00</strong> out of 5 based on <span className="rating">1</span> customer rating</span></div>
-                                                <p className="text">The passage experienced a surge in popularity during the 1960s when Letraset used it on their dry-transfer sheets, and again during the 90s as desktop publishers bundled the text with their software.</p>
-                                             </div>
-                                          </div>
-                                       </li>
-                                    </ul>
-                                 </div>
-                                 {/* <div className="th-comment-form">
-                                    <div className="form-title">
-                                       <h3 className="blog-inner-title">Add a review</h3>
-                                    </div>
-                                    <div className="row">
-                                       <div className="form-group rating-select d-flex align-items-center">
-                                          <label>Your Rating</label>
-                                          <p className="stars"><span><a className="star-1" href="#">1</a> <a className="star-2" href="#">2</a> <a className="star-3" href="#">3</a> <a className="star-4" href="#">4</a> <a className="star-5" href="#">5</a></span></p>
-                                       </div>
-                                       <div className="col-12 form-group"><textarea placeholder="Write a Message" className="form-control"></textarea> <i className="text-title far fa-pencil-alt"></i></div>
-                                       <div className="col-md-6 form-group"><input type="text" placeholder="Your Name" className="form-control" /> <i className="text-title far fa-user"></i></div>
-                                       <div className="col-md-6 form-group"><input type="text" placeholder="Your Email" className="form-control" /> <i className="text-title far fa-envelope"></i></div>
-                                       <div className="col-12 form-group"><input id="reviewcheck" name="reviewcheck" type="checkbox" /> <label htmlFor="reviewcheck">Save my name, email, and website in this browser for the next time I comment.<span className="checkmark"></span></label></div>
-                                       <div className="col-12 form-group mb-0"><button className="th-btn">Post Review <i className="far fa-arrow-right ms-1"></i></button></div>
-                                    </div>
-                                 </div> */}
-                              </div>
-                           </div>
+                           <div className="row slider-shadow th-carousel course-slider-1" data-slide-show="4" data-ml-slide-show="3" data-lg-slide-show="3" data-md-slide-show="2" data-sm-slide-show="1" data-arrows="true">
+               <div className="col-md-6 col-lg-4">
+                  <div className="course-box">
+                     <div className="course-img"><img src="assets/img/course/Course1.png" alt="img" />  </div>
+                     <div className="course-content">
+                       
+                        <h3 className="course-title"><Link to="prep-module">Pocket IPM Test Series</Link></h3>
+                        <div className="course-meta"><span><i className="fal fa-file"></i>10 weeks</span> <span><i className="fal fa-user"></i>Students 60+</span> <span><i className="fal fa-chart-simple"></i>Beginner</span></div>
+                        <div className="course-author">
+                           <div className="author-info"><a href="#" className="author-name">RV Sir</a></div>
+                           <div className="offer-tag"> </div>
                         </div>
                      </div>
                   </div>
                </div>
-            </div>
+               <div className="col-md-6 col-lg-4">
+                  <div className="course-box">
+                     <div className="course-img"><img src="assets/img/course/course2.png" alt="img" />  </div>
+                     <div className="course-content">
+                       
+                        <h3 className="course-title"><Link to="/achivers">Prime IPM Test Series</Link></h3>
+                        <div className="course-meta"><span><i className="fal fa-file"></i>10 Weeks</span> <span><i className="fal fa-user"></i>Students 50+</span> <span><i className="fal fa-chart-simple"></i>Beginner</span></div>
+                        <div className="course-author">
+                           <div className="author-info"> <a href="#" className="author-name">RV Sir</a></div>
+                           <div className="offer-tag"> </div>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+               <div className="col-md-6 col-lg-4">
+                  <div className="course-box">
+                     <div className="course-img"><img src="assets/img/course/course3.png" alt="img" />  </div>
+                     <div className="course-content">
+                       
+                        <h3 className="course-title"><Link to="/master">Premium IPM Test series</Link></h3>
+                        <div className="course-meta"><span><i className="fal fa-file"></i>10 Weeks</span> <span><i className="fal fa-user"></i>Students 30+</span> <span><i className="fal fa-chart-simple"></i>Beginner</span></div>
+                        <div className="course-author">
+                           <div className="author-info"> <a href="#" className="author-name">RV Sir</a></div>
+                           <div className="offer-tag"> </div>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+              
+               </div>
+                     
+                           </div>
+
+
+
+
+
+
+                           <div className="tab-pane fade" id="reviews" role="tabpanel" aria-labelledby="reviews-tab">
+                              
+                           <div className="row slider-shadow th-carousel course-slider-1" data-slide-show="4" data-ml-slide-show="3" data-lg-slide-show="3" data-md-slide-show="2" data-sm-slide-show="1" data-arrows="true">
+               <div className="col-md-6 col-lg-4">
+                  <div className="course-box">
+                     <div className="course-img"><img src="assets/img/course/Course1.png" alt="img" />  </div>
+                     <div className="course-content">
+                       
+                        <h3 className="course-title"><Link to="prep-module">Premium Mentorship Course</Link></h3>
+                        <div className="course-meta"><span><i className="fal fa-file"></i>10 weeks</span> <span><i className="fal fa-user"></i>Students 60+</span> <span><i className="fal fa-chart-simple"></i>Beginner</span></div>
+                        <div className="course-author">
+                           <div className="author-info"><a href="#" className="author-name">RV Sir</a></div>
+                           <div className="offer-tag"> </div>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+               <div className="col-md-6 col-lg-4">
+                  <div className="course-box">
+                     <div className="course-img"><img src="assets/img/course/course2.png" alt="img" />  </div>
+                     <div className="course-content">
+                        
+                        <h3 className="course-title"><Link to="/achivers">Subjectwise Mentorship</Link></h3>
+                        <div className="course-meta"><span><i className="fal fa-file"></i>10 Weeks</span> <span><i className="fal fa-user"></i>Students 50+</span> <span><i className="fal fa-chart-simple"></i>Beginner</span></div>
+                        <div className="course-author">
+                           <div className="author-info"> <a href="#" className="author-name">RV Sir</a></div>
+                           <div className="offer-tag"> </div>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+               <div className="col-md-6 col-lg-4">
+                  <div className="course-box">
+                     <div className="course-img"><img src="assets/img/course/course3.png" alt="img" />  </div>
+                     <div className="course-content">
+                        
+                        <h3 className="course-title"><Link to="/master">Subjectwise Mentorship</Link></h3>
+                        <div className="course-meta"><span><i className="fal fa-file"></i>10 Weeks</span> <span><i className="fal fa-user"></i>Students 30+</span> <span><i className="fal fa-chart-simple"></i>Beginner</span></div>
+                        <div className="course-author">
+                           <div className="author-info"> <a href="#" className="author-name">RV Sir</a></div>
+                           <div className="offer-tag"> </div>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+              <div className="col-md-6 col-lg-4">
+                  <div className="course-box">
+                     <div className="course-img"><img src="assets/img/course/course3.png" alt="img" />  </div>
+                     <div className="course-content">
+                       
+                        <h3 className="course-title"><Link to="/master">Mocktest Mentorship Program</Link></h3>
+                        <div className="course-meta"><span><i className="fal fa-file"></i>10 Weeks</span> <span><i className="fal fa-user"></i>Students 30+</span> <span><i className="fal fa-chart-simple"></i>Beginner</span></div>
+                        <div className="course-author">
+                           <div className="author-info"> <a href="#" className="author-name">RV Sir</a></div>
+                           <div className="offer-tag"> </div>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+              
+               </div>
+             </div> 
+
+
+           </div>
+         </div>
+       </div>
+                </div>
+                </div>
+
+
+
+                
          </div>
 
 
