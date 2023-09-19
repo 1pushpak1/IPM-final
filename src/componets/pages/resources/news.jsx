ccb35f9ea3111bff2c0b6756f2f2801c0a14057b
Pushpak <pushpakkumawat20028@gmail.com>
2023-09-20 00:38:02 +0530

    Update news.jsx

--- src/componets/pages/resources/news.jsx
+++ src/componets/pages/resources/news.jsx
@@ -37,7 +37,7 @@ function News() {
                               </svg>
                            </div>
                            <div className="testi-box_content">
-                              <div className="testi-box_img"><img src="assets/img/testimonial/testi_1.png" alt="Avater" /></div>
+                              
                               <p className="testi-box_text">“lorem ipsum”</p>
                            </div>
                            <div className="testi-box_bottom">
@@ -57,7 +57,7 @@ function News() {
                               </svg>
                            </div>
                            <div className="testi-box_content">
-                              <div className="testi-box_img"><img src="assets/img/testimonial/testi_1.png" alt="Avater" /></div>
+                              
                               <p className="testi-box_text">“Quickly maximize visionary solutions after mission critical action items productivate premium portals for impactful -services stinctively negotiate enabled niche markets via growth strategies”</p>
                            </div>
                            <div className="testi-box_bottom">
