67dd97b5796da6504bb1690baf32aaff84a1b6d6
Pushpak <pushpakkumawat20028@gmail.com>
2023-10-11 09:36:50 +0530

    Update Banner.jsx

--- src/componets/pages/home/Banner.jsx
+++ src/componets/pages/home/Banner.jsx
@@ -17,7 +17,7 @@ function Banner() {
                      <div className="col-md-6">
                         <div className="hero-style1">
                            <span className="hero-subtitle text-white" data-ani="slideinleft" data-ani-delay="0.1s"><span>IPM PRIME</span>PREMIUM MENTORSHIP PROGRAM</span>
-                           <h1 className="hero-title text-white" data-ani="slideinleft" data-ani-delay="0.4s">Unlimited IPM-AT & PI  <span className="text-theme">One-To-One Mentorship Sessions.</span></h1>
+                           <h1 className="hero-title text-white" data-ani="slideinleft" data-ani-delay="0.4s">Unlimited IPM-AT & PI \n  <span className="text-theme">One-To-One Mentorship Sessions.</span></h1>
                            <p className="hero-text" data-ani="slideinleft" data-ani-delay="0.6s">SWOT Analysis, Beginners' Asessment & Counselling, Personalised Mentorship, One-To-One expert sessions, Highly Experienced Faculties, IIM allumni Mentors, Regular Mock Tests and detailed analysis, Customised and personal study plan suited to the individual students</p>
                            <p className="hero-text" data-ani="slideinleft" data-ani-delay="0.6s">Experience your Personalised Mentorship Journey?</p>
                            <div className="btn-group" data-ani="slideinleft" data-ani-delay="0.8s"><Link to="/contactus" className="th-btn ">Book a Counselling Session<i className="fas fa-arrow-right ms-2"></i></Link></div>
