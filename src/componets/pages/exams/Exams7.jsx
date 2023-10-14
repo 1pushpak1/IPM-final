0cdb31355bc7ac77122ce4e41968cfeb089ea759
Pushpak <pushpakkumawat20028@gmail.com>
2023-10-15 00:30:32 +0530

    update

--- src/componets/pages/exams/Exams7.jsx
+++ src/componets/pages/exams/Exams7.jsx
@@ -26,7 +26,7 @@ function Exams7() {
                      <div className="course-single-top">
                         {/* <div className="course-img"><img src="assets/img/course/course-details.png" alt="Course Image" /> </div> */}
                         {/* <div className="course-meta style2"><span><i className="fal fa-file"></i>Lesson 8</span> <span><i className="fal fa-chart-simple"></i>Beginner</span></div> */}
-                        <h2 className="course-title">Five Year Integrated Programme in Management (IPM)</h2>
+                        <h2 className="course-title">Five Year Integrated Programme in Management (NPAT)</h2>
                         <ul className="course-single-meta">
                            {/* <li className="course-single-meta-author"><img src="assets/img/course/author2.png" alt="author" /> <span><span className="meta-title">Instructor: </span><a href="#">Max Alexix</a></span></li>
                            <li><span className="meta-title">Last Update: </span><a href="#">20 Jun, 2023</a></li> */}
--- src/componets/pages/exams/Exams8.jsx
+++ src/componets/pages/exams/Exams8.jsx
@@ -10,10 +10,10 @@ function Exams8() {
          <div className="shape-mockup breadcumb-shape3 jump-reverse d-lg-block d-none" data-left="50px" data-bottom="80px"><img src="assets/img/bg/breadcumb_shape_1_3.png" alt="shape" /></div>
          <div className="container">
             <div className="breadcumb-content text-center">
-               <h1 className="breadcumb-title">IIMNAT / IIM Nagpur</h1>
+               <h1 className="breadcumb-title">Set / Symbiosis</h1>
                <ul className="breadcumb-menu">
                   <li><a href="#">Exams</a></li>
-                  <li>IIM Nagpur</li>
+                  <li>Symbiosis</li>
                </ul>
             </div>
          </div>
@@ -26,7 +26,7 @@ function Exams8() {
                      <div className="course-single-top">
                         {/* <div className="course-img"><img src="assets/img/course/course-details.png" alt="Course Image" /> </div> */}
                         {/* <div className="course-meta style2"><span><i className="fal fa-file"></i>Lesson 8</span> <span><i className="fal fa-chart-simple"></i>Beginner</span></div> */}
-                        <h2 className="course-title">Five Year Integrated Programme in Management (IPM)</h2>
+                        <h2 className="course-title">Symbiosis Centre for Management Studies</h2>
                         <ul className="course-single-meta">
                            {/* <li className="course-single-meta-author"><img src="assets/img/course/author2.png" alt="author" /> <span><span className="meta-title">Instructor: </span><a href="#">Max Alexix</a></span></li>
                            <li><span className="meta-title">Last Update: </span><a href="#">20 Jun, 2023</a></li> */}
@@ -35,18 +35,19 @@ function Exams8() {
                      <div className="course-single-bottom">
                         <div className="tab-content" id="productTabContent">
                             <div className="course-description">
+                              <h5 className="h5">Course Offered </h5>
+                              <p>Bachelor of Business Administration – (BBA)</p>
                                 <h5 className="h5">ELIGIBILITY CRITERIA</h5>
-                                <p>• Candidates should have passed 10+2 examination in arts/ commerce/ science stream or equivalent with 60% (55% for candidates from SC/ ST/ PwD/ Transgender).</p>
-                                <p>• Candidates should have passed Standard XII/HSC or equivalent examination in 2021, and 2022 or appearing in 2023. The candidate must have passed the class 10th examination in the year not before 2019.                                </p>
+                                <p>The candidate should have passed XII (10+2) examination from any recognized Board with a minimum of 50% marks.</p>
                                 <h5 className="h5 mt-40">SELECTION & ADMISSION PROCESS</h5>
-                                <p>The criteria for preparing the merit list will be based on the composite score comprising the weightage of the JIPMAT score, and gender diversity as follows:</p>
-                                <p>JIPMAT Score – 95% & Gender Diversity – 5%</p>
+                                <p>1.  Selection will be based on SET (Symbiosis Entrance Test) followed by PI (Personal Interview) & WAT (Writing Ability Test).</p>
+                                <p>2.  Admission Weightage: Entrance Test score (SET/SLAT) – 50%, Personal Interview (PI) – 30% & Writing Ability Test (WAT) – 20%.</p>
                                 {/* <h5>Adminssion Weightage</h5>
                                 <p>Aptitude Test Score (ATS) – 65% AND Personal Interview (PI) – 35%</p> */}
                                 <h5>IMPORTANT DATES</h5>
-                                <p>Online Application starts from :06.04.2023</p>
-                                <p>Last date for submitting online application: 30.04.2023</p>
-                                <p>Exam Date : 28.05.2023</p>
+                                <p>Online Application starts from :16.12.2022</p>
+                                <p>Last date for submitting online application: 16.04.2023</p>
+                                <p>Exam Date : 06.05.2023</p>
                                 <p></p>
                                 <h5>EXAM PATTERN</h5>
                                 <table className="table table-striped table-hover">
@@ -61,60 +62,38 @@ function Exams8() {
                                     <tbody>
                                         <tr>
                                             <th scope="row">1</th>
-                                            <td>Quantitative Ability- MCQ</td>
-                                            <td>33</td>
-                                            <td>132</td>
+                                            <td>	General English</td>
+                                            <td>16</td>
+                                            <td>16</td>
                                         </tr>
                                         <tr>
                                             <th scope="row">2</th>
-                                            <td> Verbal Ability & Reading Comprehension</td>
-                                            <td>34</td>
-                                            <td>136</td>
+                                            <td> Quantitative Aptitude</td>
+                                            <td>16</td>
+                                            <td>16</td>
                                         </tr>
                                         <tr>
                                             <th scope="row">3</th>
-                                            <td>Data Interpretation & Logical Reasoning</td>
-                                            <td>33</td>
-                                            <td>132</td>
+                                            <td>Gereral Awarness</td>
+                                            <td>16</td>
+                                            <td>16</td>
+                                        </tr>
+                                        <tr>
+                                            <th scope="row">3</th>
+                                            <td>Analytical & Logical Reasoning</td>
+                                            <td>12</td>
+                                            <td>12</td>
                                         </tr>
                                         <tr>
                                             <th scope="row">4</th>
                                             <td>Total</td>
-                                            <td>100</td>
-                                            <td>400</td>
+                                            <td>60</td>
+                                            <td>60</td>
                                         </tr>
                                     </tbody>
                                 </table>
 
-                                <div className="title-area mb-4 mt-5">
-                                    <h2 className="sec-title mb-0">Frequently Asked Question?</h2>
-                                </div>
-                                <div className="accordion-area accordion" id="faqAccordion">
-                                    <div className="accordion-card">
-                                        <div className="accordion-header" id="collapse-item-1"><button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapse-1" aria-expanded="false" aria-controls="collapse-1">What Is The Duration Of The IPM Course At IIM Jammu?</button></div>
-                                        <div id="collapse-1" className="accordion-collapse collapse" aria-labelledby="collapse-item-1" data-bs-parent="#faqAccordion">
-                                            <div className="accordion-body">
-                                                <p className="faq-text">The Integrated Program in Management (IPM) course at IIM Jammu has a duration of 5 years. It is a full-time program that combines undergraduate and postgraduate studies in management.</p>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div className="accordion-card active">
-                                    <div className="accordion-header" id="collapse-item-2"><button className="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapse-2" aria-expanded="true" aria-controls="collapse-2">What Is The Detailed Selection Process For Admission To The IPM Course At IIM Jammu?</button></div>
-                                    <div id="collapse-2" className="accordion-collapse collapse show" aria-labelledby="collapse-item-2" data-bs-parent="#faqAccordion">
-                                        <div className="accordion-body">
-                                            <p className="faq-text">The selection process for the IPM course at IIM Jammu typically consists of two stages. The first stage involves an Aptitude Test, which assesses the candidates' quantitative ability, verbal ability, and logical reasoning skills. The shortlisted candidates from the Aptitude Test then proceed to the second stage, which includes a Written Ability Test (WAT) and a Personal Interview (PI). The final selection is based on the candidates' performance in all these stages</p>
-                                        </div>
-                                    </div>
-                                    </div>
-                                    <div className="accordion-card">
-                                    <div className="accordion-header" id="collapse-item-3"><button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapse-3" aria-expanded="false" aria-controls="collapse-3">What Are The Career Prospects After Completing The IPM Course At IIM Jammu?</button></div>
-                                    <div id="collapse-3" className="accordion-collapse collapse" aria-labelledby="collapse-item-3" data-bs-parent="#faqAccordion">
-                                        <div className="accordion-body">
-                                            <p className="faq-text">Upon completion of the IPM course at IIM Jammu, graduates have a range of career options available to them. They can pursue managerial roles in various industries, such as finance, marketing, consulting, operations, and human resources. Some graduates may choose to start their own businesses or work in entrepreneurship-related roles. Additionally, the IPM course also provides a strong foundation for further academic pursuits like pursuing a Ph.D. or other advanced degrees in management.</p>
-                                        </div>
-                                    </div>
-                                    </div>
-                                </div>
+                                
 {/* 
                                 <h5 className="h5 mt-40">Certification</h5>
                                 <p>It is designed to validate the knowledge, skills, and competencies of individuals in a specific area of study or professional field. The certification program is meticulously designed to ensure that candidates have acquired a comprehensive understanding of the subject matter. It encompasses both theoretical knowledge and practical application, allowing candidates to demonstrate their expertise in real-world scenarios.</p> */}
