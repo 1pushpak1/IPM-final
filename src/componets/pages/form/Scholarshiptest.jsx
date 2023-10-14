24e98e62d609fa157c1394d55579bf6c68c9d5bb
Pushpak <pushpakkumawat20028@gmail.com>
2023-10-15 01:47:51 +0530

    update

--- src/componets/pages/form/Scholarshiptest.jsx
+++ src/componets/pages/form/Scholarshiptest.jsx
@@ -11,7 +11,7 @@ function Scholarshiptest() {
         const form = e.target;
 
         if (form.checkValidity()) {
-            emailjs.sendForm('service_8olz9ou', 'template_etwgagg', form, 'odxm-Azw3lZvCNiOamME8')
+            emailjs.sendForm('service_8olz9ou', 'template_etwgagg', ipm, '_WTQnIBSrnhLNcbOB')
                 .then((result) => {
                     console.log(result.text);
                     formRef.current.reset();
@@ -53,7 +53,7 @@ function Scholarshiptest() {
                             </select>
                 {/* <div className="form-group"><textarea name="message" id="message" cols="30" rows="3" className="form-control style-white" placeholder="Write Your Message*" required></textarea> <i className="fal fa-pen"></i></div> */}
             </div>
-            <div className="form-btn col-12 mt-10"><button type="submit" className="th-btn">Book Your Slot<i className="fas fa-long-arrow-right ms-2"></i></button></div>
+            <div className="form-btn col-12 mt-10"><button type="submit" value="Submit" className="th-btn">Book Your Slot<i className="fas fa-long-arrow-right ms-2"></i></button></div>
         </div>
         <p className="form-messages mb-0 mt-3"></p>
         </form>
