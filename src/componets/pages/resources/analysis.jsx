c01eb8c5ae09eac15f66626ba43cd342e6430a35
Pushpak <pushpakkumawat20028@gmail.com>
2023-10-15 00:42:28 +0530

    Update analysis.jsx

--- src/componets/pages/resources/analysis.jsx
+++ src/componets/pages/resources/analysis.jsx
@@ -30,7 +30,17 @@ function Analysis() {
                <div className="col-xxl-9 col-lg-8">
                   <div className="course-single">
                      <div className="course-single-bottom">
-                     
+                     <div className="course-single">
+                     <div className="course-single-top">
+                        {/* <div className="course-img"><img src="assets/img/course/course-details.png" alt="Course Image" /> </div> */}
+                        {/* <div className="course-meta style2"><span><i className="fal fa-file"></i>Lesson 8</span> <span><i className="fal fa-chart-simple"></i>Beginner</span></div> */}
+                        <h2 className="course-title">Management Exam Analysis</h2>
+                        <ul className="course-single-meta">
+                           {/* <li className="course-single-meta-author"><img src="assets/img/course/author2.png" alt="author" /> <span><span className="meta-title">Instructor: </span><a href="#">Max Alexix</a></span></li>
+                           <li><span className="meta-title">Last Update: </span><a href="#">20 Jun, 2023</a></li> */}
+                        </ul>
+                     </div>
+                     </div>
                         {/* <div className="course-img"><img src="assets/img/course/course-details.png" alt="Course Image" /> </div> */}
                         {/* <div className="course-meta style2"><span><i className="fal fa-file"></i>Lesson 8</span> <span><i className="fal fa-chart-simple"></i>Beginner</span></div> */}
                         {/* <h2 className="course-title">Five Year Integrated Programme in Management (IPM)</h2>
