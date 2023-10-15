ce2c0065fb3767b09be0da0fe95d63927251ff9d
Pushpak <pushpakkumawat20028@gmail.com>
2023-10-15 22:59:46 +0530

    Update Home.jsx

--- src/componets/pages/home/Home.jsx
+++ src/componets/pages/home/Home.jsx
@@ -491,7 +491,7 @@ function Home() {
                               
                            </div>
                            <div className="testi-box_content">
-                              <div className="testi-box_img"><img src="assets/img/testimonial/testi_1_1.jpg" alt="Avater" /></div>
+                           
                               <p className="testi-box_text">“I give full credit to the IPM Prime team for my success. I cannot thank IPM Prime and Rajeev Sir enough. The mentors helped a lot in sharpening all the concepts. Mock Tests, Detailed mock analysis, study material and especially the Personalised sessions and Schedule plan which they prepared for us helped me a lot in quality preparation. While appearing for the exam, I felt like repeating the Mock Tests of IPM Prime and wondered if the exam had copied IPM Prime’s Mock. ”</p>
                            </div>
                            <div className="testi-box_bottom">
