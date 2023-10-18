f76cb831df52d3c467da5f139bcd9a72353d4b63
Pushpak <pushpakkumawat20028@gmail.com>
2023-10-18 08:08:31 +0530

    Update blog.jsx

--- src/componets/pages/resources/blog.jsx
+++ src/componets/pages/resources/blog.jsx
@@ -81,53 +81,7 @@ function Blog() {
        
       
       </section>
-      <section>
-         <div className="container">
-            <div className="row">
-               <div className="col-xxl-9 col-lg-8">
-                  <div className="course-single">
-                     <div className="course-single-bottom">
-                     
-                        {/* <div className="course-img"><img src="assets/img/course/course-details.png" alt="Course Image" /> </div> */}
-                        {/* <div className="course-meta style2"><span><i className="fal fa-file"></i>Lesson 8</span> <span><i className="fal fa-chart-simple"></i>Beginner</span></div> */}
-                        {/* <h2 className="course-title">Five Year Integrated Programme in Management (IPM)</h2>
-                        <ul className="course-single-meta"> */}
-                           {/* <li className="course-single-meta-author"><img src="assets/img/course/author2.png" alt="author" /> <span><span className="meta-title">Instructor: </span><a href="#">Max Alexix</a></span></li>
-                           <li><span className="meta-title">Last Update: </span><a href="#">20 Jun, 2023</a></li> */}
-                        {/* </ul> */}
-                       
-                     
-                        <ul className="nav course-tab" id="courseTab" role="tablist">
-                           {/* <li className="nav-item" role="presentation"><a className="nav-link active" id="description-tab" data-bs-toggle="tab" href="#Coursedescription" role="tab" aria-controls="Coursedescription" aria-selected="true"><i className="fa-regular fa-bookmark"></i>Premium mentorship</a></li>
-                           <li className="nav-item" role="presentation"><a className="nav-link" id="curriculam-tab" data-bs-toggle="tab" href="#curriculam" role="tab" aria-controls="curriculam" aria-selected="false"><i className="fa-regular fa-book"></i>FREE Session</a></li> */}
-                           {/* <li className="nav-item" role="presentation"><a className="nav-link" id="instructor-tab" data-bs-toggle="tab" href="#instructor" role="tab" aria-controls="instructor" aria-selected="false"><i className="fa-regular fa-user"></i>Your Mentors</a></li>
-                           <li className="nav-item" role="presentation"><a className="nav-link" id="reviews-tab" data-bs-toggle="tab" href="#reviews" role="tab" aria-controls="reviews" aria-selected="false"><i className="fa-regular fa-star-sharp"></i>Testimonial</a></li> */}
-                        </ul>
-                        <div className="tab-content" id="productTabContent">
-                           <div className="tab-pane fade show active" id="Coursedescription" role="tabpanel" aria-labelledby="description-tab">
-                              <div className="course-description">
-                                 <h5 className="h5">How to prepare Verbal ability section for IPMAT:</h5>
-                                 {/* <p>Professionally expedite synergistic technology without out-of-the-box human capital. Enthusiastically coordinate state of the art leadership after professional manufactured products. Distinctively enhance future-proof e-services whereas functionalized partnerships. Quickly streamline focused paradigms via orthogonal "outside the box" thinking. Rapidiously administrate 2.0 total linkage for cross-platform channels.</p> */}
-                                 <p>To prepare for the Verbal Ability section of the IPMAT (Integrated Program in Management - Aptitude Test), you need to focus on improving your reading, vocabulary, correct usage and comprehension skills. This section typically consists of questions related to reading comprehension, vocabulary, grammar, and sentence correction.
-</p>
-<h6>VARC for IPMAT Indore, Rohtak and JIPMAT</h6>
-<p>While the difficulty level, length and pattern of the questions might be the same, find below certain differences between the three tests: 
-</p>
-                                
-                                    <Link to="/#">More.</Link> 
-                                    
-                              </div>
-                           </div>
-                        </div>
-                     </div>
-                  </div>
-               </div>
-              
-            </div>
-         </div>
-       
       
-      </section>
 
     </>
   )
