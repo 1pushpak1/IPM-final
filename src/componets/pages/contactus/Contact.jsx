ac4bfa409da6cff1b445cbf47cc3cdb191eea34b
Pushpak <pushpakkumawat20028@gmail.com>
2023-10-19 22:48:00 +0530

    Revert "Update Contact.jsx"
    This reverts commit 8b0d92ca04c448c5c13dd0edc0474fe1b9de50e2.

--- src/componets/pages/contactus/Contact.jsx
+++ src/componets/pages/contactus/Contact.jsx
@@ -5,7 +5,20 @@ import { Link } from 'react-router-dom';
 function Contact() {
   return (
     <>
-   
+    <div className="breadcumb-wrapper" data-bg-src="assets/img/bg/breadcumb-bg.jpg" data-overlay="title" data-opacity="8">
+         <div className="breadcumb-shape" data-bg-src="assets/img/bg/breadcumb_shape_1_1.png"></div>
+         <div className="shape-mockup breadcumb-shape2 jump d-lg-block d-none" data-right="30px" data-bottom="30px"><img src="assets/img/bg/breadcumb_shape_1_2.png" alt="shape" /></div>
+         <div className="shape-mockup breadcumb-shape3 jump-reverse d-lg-block d-none" data-left="50px" data-bottom="80px"><img src="assets/img/bg/breadcumb_shape_1_3.png" alt="shape" /></div>
+         <div className="container">
+            <div className="breadcumb-content text-center">
+               <h1 className="breadcumb-title">Contact Us</h1>
+               <ul className="breadcumb-menu">
+                  <li><Link to="/">Home</Link></li>
+                  <li>Contact Us</li>
+               </ul>
+            </div>
+         </div>
+      </div>
       <div className="space" id="contact-sec">
          <div className="container">
             {/* <div className="map-sec">
