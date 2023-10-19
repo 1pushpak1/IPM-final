5f3ed8c590b32fe949ea55236862b8a52b25e911
Pushpak <pushpakkumawat20028@gmail.com>
2023-10-19 22:30:25 +0530

    Update Home.jsx

--- src/componets/pages/home/Home.jsx
+++ src/componets/pages/home/Home.jsx
@@ -4,8 +4,6 @@ import "react-responsive-carousel/lib/styles/carousel.min.css"
 import { Carousel } from 'react-responsive-carousel'
 import Banner from './Banner'
 import { Link } from 'react-router-dom';
-import { Modal } from '@weahead/react-customizable-modal-vite'
-import Contact from '../contactus/Contact.jsx'
 function Home() {
    return (
       <>
@@ -248,37 +246,7 @@ function Home() {
                            <h2 className="cta-title sec-title">Get Personalized Mentorship Program</h2>
 
                         </div>
-                        <button onClick={() => { setIsOpen(true) }} className="th-btn style8">Book a Counselling Session<i className="fas fa-arrow-right ms-1"></i></button>
-                        <Modal
-                     isOpen={isOpen}
-                     onEscape={() => {
-                        setIsOpen(false)
-                     }}
-                     onOverlayClick={() => {
-                        setIsOpen(false)
-                     }}
-                  >
-                     <div style={{ background: "white", padding: 10, marginRight: -10}}>
-                        <Contact />
-                     </div>
-                     <button
-                        style={{
-                           position: "absolute",
-                           top: 30,
-                           right: 30,
-                           zIndex: 101,
-                           background: "none",
-                           border: "none",
-                           color: "black",
-                           fontSize: 30
-                        }}
-                        onClick={() => {
-                           setIsOpen(false)
-                        }}
-                     >
-                        <i class="fa-solid fa-circle-xmark"></i>
-                     </button>
-                  </Modal>
+                        <Link to="/contactus" className="th-btn style8">Book a Counselling Session<i className="fas fa-arrow-right ms-1"></i></Link>
                      </div>
                   </div>
                </div>
