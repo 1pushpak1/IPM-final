abcb597f44bb09c43bc6ffa921a49ff7fb0eb804
Pushpak <pushpakkumawat20028@gmail.com>
2023-10-21 01:04:39 +0530

    Update Home.jsx

--- src/componets/pages/home/Home.jsx
+++ src/componets/pages/home/Home.jsx
@@ -1,5 +1,4 @@
-import { React, Component } from 'react'
-import { React, useState } from 'react'
+import { React, Component , useState} from 'react'
 import ReactDOM from 'react-dom'
 import "react-responsive-carousel/lib/styles/carousel.min.css"
 import { Carousel } from 'react-responsive-carousel'
