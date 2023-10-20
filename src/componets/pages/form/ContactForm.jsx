0feba6c6dc1db155e081867c746bc9bb8b7d2f6e
Pushpak <pushpakkumawat20028@gmail.com>
2023-10-21 01:41:04 +0530

    Update ContactForm.jsx

--- src/componets/pages/form/ContactForm.jsx
+++ src/componets/pages/form/ContactForm.jsx
@@ -35,14 +35,18 @@ function ContactForm() {
                 <div className="form-group"><input type="email" className="form-control style-white" name="email" id="email" placeholder="Email Address*" required /> <i className="fal fa-envelope"></i></div>
             </div>
             <div className="col-md-6">
-                <div className="form-group"><input type="subject" className="form-control style-white" name="subject" id="subject" placeholder="Subject*" required /> <i className="fal fa-book"></i></div>
+                <div className="form-group"><input type="school" className="form-control style-white" name="school" id="school" placeholder="School*" required /> <i className="fal fa-book"></i></div>
             </div>
             <div className="col-md-6">
                 <div className="form-group"><input type="tel" className="form-control style-white" name="number" id="number" placeholder="Phone Number*" required /> <i className="fal fa-phone"></i></div>
             </div>
-            <div className="col-12">
-                <div className="form-group"><textarea name="message" id="message" cols="30" rows="3" className="form-control style-white" placeholder="Write Your Message*" required></textarea> <i className="fal fa-pen"></i></div>
+            <div className="col-md-6">
+                <div className="form-group"><input type="class" className="form-control style-white" name="class" id="class" placeholder="Class*" required /> <i className="fal fa-phone"></i></div>
             </div>
+            <div className="col-md-6">
+                <div className="form-group"><input type="city" className="form-control style-white" name="city" id="city" placeholder="City*" required /> <i className="fal fa-phone"></i></div>
+            </div>
+           
             <div className="form-btn col-12 mt-10"><button type="submit" value="Submit" className="th-btn">Send Message<i className="fas fa-long-arrow-right ms-2"></i></button></div>
         </div>
         <p className="form-messages mb-0 mt-3"></p>
