1733f7f954117c1256413f9ae356fec90ab09e3d
Pushpak <pushpakkumawat20028@gmail.com>
2023-10-21 00:52:22 +0530

    Update Contact.jsx

--- src/componets/pages/contactus/Contact.jsx
+++ src/componets/pages/contactus/Contact.jsx
@@ -30,6 +30,7 @@ function Contact() {
                <div className="col-xl-5 mb-30 mb-xl-0">
                   <div className="me-xxl-5 mt-40">
                   <div className="img1"><img src="assets/img/icon/book-consultation.svg" alt="img" /></div>
+                  <div className="space-top"></div>
                      <div className="title-area mb-25">
                         <h2 className="border-title h3"> Book a Consultation</h2>
                      </div>
