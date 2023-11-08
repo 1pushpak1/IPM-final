2f29d0610014d419a7c6262d60662dd157f7af59
Pushpak <pushpakkumawat20028@gmail.com>
2023-11-08 19:23:54 +0530

    Update Banner.jsx

--- src/componets/pages/home/Banner.jsx
+++ src/componets/pages/home/Banner.jsx
@@ -42,7 +42,31 @@ function Banner() {
                         setIsOpen(false)
                      }}
                   >
-                     <div style={{ background: "white", padding: 1, marginRight: -1}}>
+                     <div style={{
+    overlay: {
+      position: 'fixed',
+      top: 0,
+      left: 0,
+      right: 0,
+      bottom: 0,
+      backgroundColor: 'rgba(255, 255, 255, 0.75)'
+    },
+    content: {
+      position: 'absolute',
+      top: '40px',
+      left: '40px',
+      right: '40px',
+      bottom: '40px',
+      border: '1px solid #ccc',
+      background: '#fff',
+      overflow: 'auto',
+      WebkitOverflowScrolling: 'touch',
+      borderRadius: '4px',
+      outline: 'none',
+      padding: '20px'
+    }
+  }}>
+                     {/* style={{ background: "white", padding: 1, marginRight: -1}}> */}
                         <Contact />
                      </div>
                      {/* <button
