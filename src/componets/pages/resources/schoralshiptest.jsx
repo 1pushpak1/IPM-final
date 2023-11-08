653c991fb0e00af303262862947ed67764fe1fb5
Pushpak <pushpakkumawat20028@gmail.com>
2023-11-08 22:10:01 +0530

    update

--- src/componets/pages/resources/schoralshiptest.jsx
+++ src/componets/pages/resources/schoralshiptest.jsx
@@ -1,13 +1,48 @@
 import { React, useState } from 'react'
 import BookFreeDemo from '../form/BookFreeDemo'
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
 import { Modal } from '@weahead/react-customizable-modal-vite'
-import Contact from '../contactus/Contact.jsx'
 
 function Scholarshiptest() {
+   const [isOpen, setIsOpen] = useState(false);
    return (
       <>
-         <section>
+         <div className="breadcumb-wrapper" data-overlay="title" data-opacity="8">
+            <div className="breadcumb-shape" data-bg-src="assets/img/bg/breadcumb_shape_1_1.png"></div>
+            <div className="shape-mockup breadcumb-shape2 jump d-lg-block d-none" data-right="30px" data-bottom="30px"><img src="assets/img/bg/breadcumb_shape_1_2.png" alt="shape" /></div>
+            <div className="shape-mockup breadcumb-shape3 jump-reverse d-lg-block d-none" data-left="50px" data-bottom="80px"><img src="assets/img/bg/breadcumb_shape_1_3.png" alt="shape" /></div>
+            <div className="container">
+               <div className="breadcumb-content text-center">
+                  <h1 className="breadcumb-title">Scholarshiptest Program</h1>
+                 
+               </div>
+            </div>
+         </div>
+
+
+         {/* <div className="container">
+            <div className="row align-items-center">
+               <div className="col-xl-6">
+                  <div className="wcu-img-1">
+                     <div className="img1"><img src="assets/img/normal/short.png" alt="img" /></div>
+
+                  </div>
+               </div>
+               <div className="col-xl-6">
+                  <div className="wcu-wrap1">
+                     <div className="title-area mb-25">
+
+                        <h2 className="sec-title">"IPM PRIME is a Mentorship Platform not a random Coaching institute"</h2>
+                        <h5>Its Never Too Late!</h5>
+
+
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div> */}
+
+<section>
             <div className="container">
                <div className="row">
                   <div className="col-xxl-9 col-lg-8">
@@ -106,31 +141,17 @@ function Scholarshiptest() {
                         </div>
                      </div>
                   </div>
-                  <div className="col-xxl-3 col-lg-4">
-                     <BookFreeDemo />
-                  </div>
-                  <div className="cta-area-1">
-                     <div className="container">
-                        <div className="row align-items-right justify-content-between">
-                           <div className="col-lg-12">
-                              <div className="cta-wrap title-area mb-0">
-
-                                 <div className="cta-content">
-                                    <h2 className=" footer-contact_link">Have Questions ? Call us : +91-99100 88661</h2>
-
-                                 </div>
-                                 <Link to="/contactus" className="th-btn style8">Book a Counselling Session<i className="fas fa-arrow-right ms-1"></i></Link>
-                              </div>
-                           </div>
-                        </div>
-
-                     </div>
-                  </div>
+                 
+                 
                </div>
             </div>
          </section >
+
+        
+                
+
       </>
    )
 }
 
-export default Scholarshiptest;
+export default Scholarshiptest
